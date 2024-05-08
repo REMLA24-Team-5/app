f775f3b0050c1e5925bcdb3230fd5caac93ec8f2
Jurriaan <jurrbuitenweg@gmail.com>
2024-05-08 11:16:46 +0200

    added basic structure

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # app
 The application has a frontend and a service, which can, but do not have to, be implemented separately. The application uses the model service in a sensible use case.
+
---
+++ app-service/app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, render_template, request
+from connector import Connector
+
+template_dir = os.path.abspath('../app-frontend/templates')
+app = Flask(__name__, template_folder=template_dir)
+
+connector_to_model_service = Connector()
+
+@app.route("/", methods = ['GET', 'POST'])
+def index():
+    print("hey", request.method)
+    if request.method == 'GET':
+        return render_template('index.html')
+
+    if request.method == 'POST':
+        url = request.form['url_input']
+        url_info = connector_to_model_service.get_url_data(url)
+        return render_template('index.html', url_data=url_info)
